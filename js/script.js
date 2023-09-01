504de5530a9e5f211cdce79584a475b28bcf25b3
Mekafuyu <mayconbrunob@hotmail.com>
2023-09-01 19:04:11 -0300

    SOCORRO DEIXEI O TIME ERRADO

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import * as timer from './timer.js'
 
 // Data do lançamento (meses começam em 0)
-var target = new Date(2023, 8, 1, 18, 47, 0o0);
+var target = new Date(2023, 8, 1, 23, 0o0, 0o0);
 
 // Sincroniza os tempos
 timer.freeze();
